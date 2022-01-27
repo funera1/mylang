a7aa62ee240d804773557cd9b4d330ade3288bb1
funera1 <funeral7776@gmail.com>
2022-01-27 19:38:27 +0900

    update createFirstSetを一つの非終端記号について作った

--- parsing.cpp
+++ parsing.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
-#define sz(x) (x).size()
-using namespace std;
+#include "include.hpp"
+#include "global_values.hpp"
+#include "tools.cpp"
 
 void createFirstSet(){
 	// とりあえず一つの非終端記号についてだけやる
@@ -8,17 +8,14 @@ void createFirstSet(){
 	while(1){
 		set<string> firstSetTmp;
 		for(auto si : firstSet){
-			// TODO: m(名前未定):map<string, dst>, srcからdstに行けるやつを作る
-			for(auto mi : m[si]){
-				firstSetTmp.insert(mi);
+			for(auto dst_i : bnf_src_to_dst[si]){
+				firstSetTmp.insert(dst_i[0]);
 			}
 		}
 		firstSet = firstSetTmp;
 		// すべてのfirstSetの要素について終端記号かチェックする
 		bool f = true;
 		for(auto fs : firstSet){
-			// TODO: is_term(s): sが終端記号かどうかを判定する関数を作る
-			// (終端記号->1, 非終端記号->0)
 			if(!is_term(fs))f = false;
 		}
 		if(f)break;
