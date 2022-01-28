859e9ff7692cbbcff8e04c25846a5444ab3fae95
funera1 <funeral7776@gmail.com>
2022-01-29 02:36:59 +0900

    update createFirstSet一応完成(未テスト)

--- parsing.cpp
+++ parsing.cpp
@@ -3,21 +3,24 @@
 #include "tools.cpp"
 
 void createFirstSet(){
-	// とりあえず一つの非終端記号についてだけやる
-	set<string> firstSet;
-	while(1){
-		set<string> firstSetTmp;
-		for(auto si : firstSet){
-			for(auto dst_i : bnf_src_to_dst[si]){
-				firstSetTmp.insert(dst_i[0]);
+	// とりあえず一つの非終端記号についてだけやる->全ての非終端記号についてやる
+	map<string, set<string>> firstSets;
+	// 全ての非終端記号を見る
+	for(auto [src, dst] : token_trans_table){
+		// 既に見てたら飛ばす
+		if(!firstSets[src].empty())continue;
+		set<string> firstSet;
+		// 最初は自分自身の非終端記号を持ってスタート
+		firstSet.insert(src);
+		while(!all_is_term_in_set(firstSet)){
+			set<string> firstSetTmp;
+			for(auto si : firstSet){
+				for(auto dst_i : bnf_src_to_dst[si]){
+					firstSetTmp.insert(dst_i[0]);
+				}
 			}
+			firstSet = firstSetTmp;
 		}
-		firstSet = firstSetTmp;
-		// すべてのfirstSetの要素について終端記号かチェックする
-		bool f = true;
-		for(auto fs : firstSet){
-			if(!is_term(fs))f = false;
-		}
-		if(f)break;
+		firstSets[src] = firstSet;
 	}
 }
