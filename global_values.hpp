2aacd53cdd77a89dbc7ddc14640b25273feab618
funera1 <funera1@pop-os.localdomain>
2022-02-01 01:36:14 +0900

    add global_values.hpp

---
+++ global_values.hpp
@@ -0,0 +1,8 @@
+#include "include.hpp"
+// あるtokenから遷移するtoken列のリスト(NUSED+1はtokenkindの個数)
+// 例) tokenTransTable[0] = Pbnf("PROGRAM", {"LBRACKET", "COMPOUND", "RBRACKET"})
+vector<Pbnf> token_trans_table;
+// トークンの文字列から対応するtokenkindを割り当てる
+map<string, int> tokenstr_to_tokenkind;
+// 終端記号の集合
+set<string> terminal_symbols;
