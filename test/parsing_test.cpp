b170a1834d3f4dbaf0e41df1005e8bdd75e1dd1c
funera1 <funeral7776@gmail.com>
2022-02-07 17:08:25 +0900

    a

--- test/parsing_test.cpp
+++ test/parsing_test.cpp
@@ -15,29 +15,31 @@ int main(){
 
     input_stream = x.first;
     token_stream = x.second;
+    for(int i = 0; i < sz(token_stream); i++)cout << token_stream[i] << endl;
+    // assert(0);
     Parsing p(bnf_transition_list, nonterm_list);
-    token_stream.push_back("$");
     // DEBUG: first_set
-    cout << "###CHECK FIRST SET###" << endl;
-    for(auto [non_term, first_set] : p.first_sets){
-        cout << non_term << ":" << endl;
-        cout << '{';
-        for(auto t : first_set){
-            cout << t << ", ";
-        }
-        cout << '}' << endl;
-    }
+    // cout << "###CHECK FIRST SET###" << endl;
+    // for(auto [non_term, first_set] : p.first_sets){
+    //     cout << non_term << ":" << endl;
+    //     cout << '{';
+    //     for(auto t : first_set){
+    //         cout << t << ", ";
+    //     }
+    //     cout << '}' << endl;
+    // }
+    // cout << endl;
     // DEBUG: follow_setが成功してるかどうか
-    cout << endl << "###CHECK FOLLOW SET###" << endl;
-    for(auto [non_term, follow_set] : p.follow_sets){
-        cout << non_term << ":" << endl;
-        cout << '{';
-        for(auto t : follow_set){
-            cout << t << ", ";
-        }
-        cout << '}' << endl;
-    }
-    cout << endl;
+    // cout << "###CHECK FOLLOW SET###" << endl;
+    // for(auto [non_term, follow_set] : p.follow_sets){
+    //     cout << non_term << ":" << endl;
+    //     cout << '{';
+    //     for(auto t : follow_set){
+    //         cout << t << ", ";
+    //     }
+    //     cout << '}' << endl;
+    // }
+    // cout << endl;
     // DEBUG: parsing tableが成功してるかどうか
     // auto t = p.create_ll_parsing_table();
     // for(auto ti : t){
