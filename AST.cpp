78403a06fd8c0e88ac7b03b7d966f24358c08e70
funera1 <funeral7776@gmail.com>
2022-02-20 18:59:46 +0900

    それぞれのnodeの初期化関数を作成した

--- AST.cpp
+++ AST.cpp
@@ -21,6 +21,27 @@ typedef struct statement_node {
     string statement_kind;
 } statement_node;
 
+expr_node* init_expr_node(int value, char op){
+    expr_node* node = new expr_node;
+    node->value = value;
+    node->op = op;
+    node->next_node = NULL;
+    return node;
+}
+
+assign_node* init_assign_node(string name, expr_node* expr_node = NULL){
+    assign_node* node = new assign_node;
+    node->name = name;
+    node->expr_node = expr_node;
+    return node;
+}
+
+declaration_node* init_declaration_node(string type, string name){
+    declaration_node* node = new declaration_node;
+    node->type = type;
+    node->name = name;
+    return node;
+}
 
 // RSTをASTに変換する
 void RST_to_AST(node_t* root){
@@ -28,6 +49,9 @@ void RST_to_AST(node_t* root){
     node_t* now_node = root;
     statement_node* statement_root;
     while(1){
-        if(is_statement(now_node->token))
+        // nodeがstatement関連ならstatement_nodeを伸ばす
+        if(is_statement(now_node->token)){
+
+        }
     }
 }
