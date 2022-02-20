e3ffa4dbc301adc87f43a06f302a9d5e3bdcfe1c
funera1 <funeral7776@gmail.com>
2022-02-20 19:09:42 +0900

    statement_nodeの変更、初期化関数の追加

--- AST.cpp
+++ AST.cpp
@@ -19,6 +19,9 @@ typedef struct declaration_node {
 
 typedef struct statement_node {
     string statement_kind;
+    struct statement_node* next_statement_node;
+    assign_node* assign_node;
+    declaration_node* declaration_node;
 } statement_node;
 
 expr_node* init_expr_node(int value, char op){
@@ -43,15 +46,25 @@ declaration_node* init_declaration_node(string type, string name){
     return node;
 }
 
+statement_node* init_statement_node(string statement_kind){
+    statement_node* node = new statement_node;
+    node->statement_kind = statement_kind;
+    node->next_statement_node = NULL;
+    node->assign_node = NULL;
+    node->declaration_node = NULL;
+    return node;
+}
+
 // RSTをASTに変換する
 void RST_to_AST(node_t* root){
     // ASTを走査する
     node_t* now_node = root;
-    statement_node* statement_root;
+    statement_node* statement_node = init_statement_node("ROOT");
     while(1){
         // nodeがstatement関連ならstatement_nodeを伸ばす
         if(is_statement(now_node->token)){
-
+            auto next_node = init_statement_node(now_node->token);
+            statement_node->
         }
     }
 }
