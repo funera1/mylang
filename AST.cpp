71d16f7e70a7df26203f2e77cf0c80f2ff7fd25d
funera1 <funeral7776@gmail.com>
2022-02-24 23:32:45 +0900

    expr_nodeの状態の持ち方を変えた

--- AST.cpp
+++ AST.cpp
@@ -3,9 +3,11 @@
 
 // 各種ノード
 typedef struct expr_node {
+    string token;
     int value;
     char op;
-    struct expr_node* next_node;
+    struct expr_node* left_node;
+    struct expr_node* right_node;
 } expr_node;
 
 typedef struct assign_node {
@@ -26,11 +28,13 @@ typedef struct statement_node {
 } statement_node;
 
 // 各種ノードの初期化関数
-expr_node* init_expr_node(int value, char op){
+expr_node* init_expr_node(string token, int value, char op){
     expr_node* node = new expr_node;
+    node->token = token;
     node->value = value;
     node->op = op;
-    node->next_node = nullptr;
+    node->left_node = nullptr;
+    node->right_node = nullptr;
     return node;
 }
 
@@ -58,19 +62,42 @@ statement_node* init_statement_node(string statement_kind){
 }
 
 // 各種statementのノード構築
-expr_node* construct_expr_node(node_t* node){
+expr_node* construct_expr_node(nonterm_node* node){
+    // nonterm_nodeを走査して、数式(中値記法)を作る
+    nonterm_node* root = node;
+    vector<string> formula;
+    node = get_next_node(node);
+    while(root != node){
+        if(node->term_node != nullptr){
+            term_node* term_node = node->term_node;
+            // number
+            if(term_node->token == "#id"){
+                formula.push_back(to_string(term_node->number));
+            }
+            // op
+            else {
+                formula.push_back(term_node->token);
+            }
+        }
+        node = get_next_node(node);
+    }
+    
+    // 中値記法を逆ポーランド記法に変換する
+    vector<string> reverse_polish = convert_reverse_polish(formula);
+    
+    // 逆ポーランドを木にする
 }
 
 // 引数nodeのnode->tokenはASSIGN_STATEMENT
 // ASSIGN_STATEMENT
 // ID EXPR SEMICOLONの構造
-assign_node* construct_assign_node(node_t* node){
+assign_node* construct_assign_node(nonterm_node* node){
     string name = "";
     expr_node* expr_node = nullptr;
-    node_t* tmp_node = node;
+    nonterm_node* tmp_node = node;
     // ID
     tmp_node = get_adjacent_node(tmp_node, "child");
-    name = get_id_from_node_t(tmp_node);
+    name = get_id_from_nonterm_node(tmp_node);
     // expr
     tmp_node = get_adjacent_node(tmp_node, "left"); // ASIGN
     tmp_node = get_adjacent_node(tmp_node, "left"); // EXPR
@@ -82,10 +109,10 @@ assign_node* construct_assign_node(node_t* node){
 // 引数nodeのnode->tokenはDECLARATION_STATEMENT
 // DECLARATION_STATEMEN
 // INT ID SEMICOLON の構造になっている
-declaration_node* construct_declaration_node(node_t* node){
+declaration_node* construct_declaration_node(nonterm_node* node){
     string type = "";
     string name = "";
-    node_t* tmp_node = node;
+    nonterm_node* tmp_node = node;
 
     // INT
     tmp_node = get_adjacent_node(tmp_node, "child");
@@ -93,16 +120,16 @@ declaration_node* construct_declaration_node(node_t* node){
     // ID
     tmp_node = get_adjacent_node(tmp_node, "left");
     // IDやNUMBERなどの実値をRSTと結び付けられてない
-    name = get_id_from_node_t(tmp_node);
+    name = get_id_from_nonterm_node(tmp_node);
 
     declaration_node* declaration_node = init_declaration_node(type, name);
     return declaration_node;
 }
 
 // RSTをASTに変換する
-void RST_to_AST(node_t* root){
+void RST_to_AST(nonterm_node* root){
     // ASTを走査する
-    node_t* now_node = root;
+    nonterm_node* now_node = root;
     statement_node* statement_node = init_statement_node("ROOT");
     while(1){
         // nodeがstatement関連ならstatement_nodeを伸ばす
