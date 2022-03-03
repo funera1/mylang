56d4425790b9f187f941354d167a58b831f13747
funera1 <funeral7776@gmail.com>
2022-03-04 01:05:36 +0900

    if_statementを実行する機能

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -60,5 +60,13 @@ void interpreter(statement_node* root){
             // DEBUG
             cout << "DEBUG: " << name << " is " << value_int << endl;
         }
+        if(node->token == "IF_STATEMENT"){
+            auto if_node = node->if_node;
+            int value = calc_expr_node(if_node->conditional_expr_node);
+            // 条件式の中身が1以上のときのみ実行する
+            if(value > 0){
+                interpreter(if_node->statment_node);
+            }
+        }
     }
 }
