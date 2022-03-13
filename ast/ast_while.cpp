8c2c2f225290117317562e135bd455edee1fda2f
funera1 <funeral7776@gmail.com>
2022-03-13 16:01:58 +0900

    while機能追加

---
+++ ast/ast_while.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "../tools.cpp"
+#include "../RST.cpp"
+#include "ast_expr.cpp"
+#include "AST.hpp"
+
+typedef struct while_node {
+    // 条件式
+    expr_node* conditional_expr_node;
+    // 本文
+    statement_node* statment_node;
+} while_node;
+
+while_node* init_while_node(expr_node* expr_node, statement_node* statement_node){
+    while_node* node = new while_node;
+    node->conditional_expr_node = expr_node;
+    node->statment_node = statement_node;
+    return node;
+}
+
+while_node* construct_while_node(nonterm_node*& node){
+    // WHILE_STATEMENT
+    nonterm_node* tmp_node = node;
+    // WHILE 
+    tmp_node = get_adjacent_node(tmp_node, "child");
+    // LPAREN
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    // EXPR
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    expr_node* cond_expr_node = construct_expr_node(tmp_node);
+    // RPAREN
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    // LBRACKET
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    // COMPOUND
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    statement_node* statement_node = RST_to_AST(tmp_node);
+    // RBRACKET
+    tmp_node = get_adjacent_node(tmp_node, "right");
+    node = tmp_node;
+    
+    while_node* while_node = init_while_node(cond_expr_node, statement_node);
+    return while_node;
+}
--- interpreter/intp_while.cpp
+++ interpreter/intp_while.cpp
@@ -1,12 +1,13 @@
 #include "interpreter.hpp"
 
 void intp_while(statement_node* node){
-    auto if_node = node->if_node;
+    auto while_node = node->while_node;
     while(1){
-        int value = calc_expr_node(if_node->conditional_expr_node);
+        int value = calc_expr_node(while_node->conditional_expr_node);
         // 条件式の中身が1以上のときのみ実行する
+        cout << "while's value is " << value << endl;
         if(value > 0){
-            interpreter(if_node->statment_node);
+            interpreter(while_node->statment_node);
         }
         else {
             break;
