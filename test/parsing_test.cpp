c8703de4840ab48eca8be115abee7e12ccde31c3
funera1 <funeral7776@gmail.com>
2022-03-27 11:31:08 +0900

    debugを関数化した

--- test/parsing_test.cpp
+++ test/parsing_test.cpp
@@ -8,6 +8,47 @@
 #include "../ast/AST.cpp"
 #include "../interpreter/interpreter.cpp"
 
+void dbg_print_token_stream(vector<string> token_stream){
+    for(int i = 0; i < sz(token_stream); i++)cout << token_stream[i] << endl;
+}
+
+void dbg_check_first_set(Parsing p){
+    cout << "###CHECK FIRST SET###" << endl;
+    for(auto [non_term, first_set] : p.first_sets){
+        cout << non_term << ":" << endl;
+        cout << '{';
+        for(auto t : first_set){
+            cout << t << ", ";
+        }
+        cout << '}' << endl;
+    }
+    cout << endl;
+}
+
+void dbg_check_follow_set(Parsing p){
+    cout << "###CHECK FOLLOW SET###" << endl;
+    for(auto [non_term, follow_set] : p.follow_sets){
+        cout << non_term << ":" << endl;
+        cout << '{';
+        for(auto t : follow_set){
+            cout << t << ", ";
+        }
+        cout << '}' << endl;
+    }
+    cout << endl;
+}
+
+void dbg_check_parsing_table(Parsing p){
+    auto t = p.create_ll_parsing_table();
+    for(auto ti : t){
+        auto [non_term, term] = ti.first;
+        int num = ti.second;
+        if(num == -1)continue;
+        cout << non_term << " " << term << " " << num << endl;
+    }
+    cout << endl << endl;
+}
+
 int main(){
 	string str = fileToString("input.txt");
     init();
@@ -20,46 +61,15 @@ int main(){
     input_stream = x.first;
     token_stream = x.second;
     // DEBUG
-    // for(int i = 0; i < sz(token_stream); i++)cout << token_stream[i] << endl;
-    // for(auto [src, dst] : bnf_transition_list){
-    //     cout << src << ": ";
-    //     for(auto di : dst)cout << di << ", ";
-    //     cout << endl;
-    // }
+    // dbg_print_token_stream(token_stream);
     Parsing p;
 
     // DEBUG: first_set
-    // cout << "###CHECK FIRST SET###" << endl;
-    // for(auto [non_term, first_set] : p.first_sets){
-    //     cout << non_term << ":" << endl;
-    //     cout << '{';
-    //     for(auto t : first_set){
-    //         cout << t << ", ";
-    //     }
-    //     cout << '}' << endl;
-    // }
-    // cout << endl;
+    // dbg_check_first_set(p);
     // DEBUG: follow_setが成功してるかどうか
-    // cout << "###CHECK FOLLOW SET###" << endl;
-    // for(auto [non_term, follow_set] : p.follow_sets){
-    //     cout << non_term << ":" << endl;
-    //     cout << '{';
-    //     for(auto t : follow_set){
-    //         cout << t << ", ";
-    //     }
-    //     cout << '}' << endl;
-    // }
-    // cout << endl;
+    // dbg_check_follow_set(p);
     // DEBUG: parsing tableが成功してるかどうか
-    // auto t = p.create_ll_parsing_table();
-    // for(auto ti : t){
-    //     auto [non_term, term] = ti.first;
-    //     int num = ti.second;
-    //     if(num == -1)continue;
-    //     cout << non_term << " " << term << " " << num << endl;
-    // }
-    // cout << endl << endl;
-
+    // dbg_check_parsing_table(p);
     // DEBUG: 構文解析のテスト
     // p.parsing(token_stream);
     // DEBUG: RSTのテスト
