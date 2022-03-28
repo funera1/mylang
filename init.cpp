4ecb20607b48daed7eb126d610f96df2b4b9422b
funera1 <funeral7776@gmail.com>
2022-03-28 19:23:09 +0900

    bnf_transition_listに(終端記号, 空)を追加

--- init.cpp
+++ init.cpp
@@ -11,6 +11,8 @@ vector<P_src_dst> create_bnf_transtion_list(){
 	// この関数で返す値
 	vector<P_src_dst> local_bnf_transition_list;
 	string now_line = "";
+
+	set<string> term_set;
 	while(getline(input_bnf, now_line)){
 		// コメントはスルー
 		if(sz(now_line) >= 2 && now_line[0] == '/' && now_line[1] == '/')continue;
@@ -37,9 +39,8 @@ vector<P_src_dst> create_bnf_transtion_list(){
 			else if(separator == ":="){
 				// 右辺は一つのtokenで終端記号
 				string word = getNextStr(now_line, i);
-				// bnf_transition_list[term] = {}で初期化
-				bnf_transition_list[word] = vector<string>{};
 				dst.push_back(word);
+				term_set.insert(word);
 			}
 			// それ以外ならエラー
 			else {
@@ -49,20 +50,19 @@ vector<P_src_dst> create_bnf_transtion_list(){
 			// TODO: bnf_src_to_dstとbnf_transition_listどっちも共通した過程を踏むのにbnf_transition_listのための関数になっているので調整する
 			bnf_src_to_dst[src].push_back(dst);
 			local_bnf_transition_list.push_back(P_src_dst(src, dst));
+
+			// bnf_transition_list[term] = {}で初期化
+			for(auto t : term_set){
+				bnf_transition_list.push_back(P_src_dst(t, vector<string>{}));
+			}
 		}
 	}
 	return local_bnf_transition_list;
 }
 
-void construct_term_and_nonterm_set(){
-	for(auto [src, dst] : bnf_transition_list){
-		term_and_nonterm_set.insert(dst);
-	}
-}
 
 // 変数の初期化
 void init(){
 	bnf_transition_list = create_bnf_transtion_list();
-	construct_term_and_nonterm_set();
 	create_nonterm_and_term_list();
 }
