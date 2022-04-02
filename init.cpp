36375e49a140c1c41a14777065b2f84945551cdb
funera1 <funeral7776@gmail.com>
2022-04-02 19:31:06 +0900

    ebnf対応

--- init.cpp
+++ init.cpp
@@ -5,17 +5,15 @@
 
 
 // 文法の遷移を3重vectorに写す
-vector<P_src_dst> create_bnf_transtion_list(){
+void> create_bnf_transtion_list(){
 	ifstream input_bnf = fileToIfstream(home_dir+"bnf");
 
 	// この関数で返す値
-	vector<P_src_dst> local_bnf_transition_list;
 	string now_line = "";
 
 	set<string> term_set;
 	while(getline(input_bnf, now_line)){
 		// コメントはスルー
-		// if(sz(now_line) >= 2 && now_line[0] == '/' && now_line[1] == '/')continue;
 		if(sz(now_line) >= 1 && now_line[0] == ';')continue;
 		// 空行もスルー
 		if(now_line == "")continue;
@@ -30,27 +28,28 @@ vector<P_src_dst> create_bnf_transtion_list(){
 			// separator( ::= or := )を取得
 			string separator = getNextStr(now_line, i);
 			// ::=のとき
-			if(separator == "::="){
+			if(separator == "="){
 				while(i < sz(now_line)){
 					string word = getNextStr(now_line, i);
+					// 区切り
+					if(word == "\\"){
+						bnf_transition_list(P_src_dst(src, dst));
+						dst = vector<string>();
+					}
+					// 終端記号(""か''で囲まれているもの)
+					if((word[0] == '"' && word[sz(word)-1] == '"') || (word[0] == "'" && word[sz(word)-1] == "'")){
+						term_set.insert(word);
+					}
 					dst.push_back(word);
 				}
 			}
-			// :=のとき
-			else if(separator == ":="){
-				// 右辺は一つのtokenで終端記号
-				string word = getNextStr(now_line, i);
-				dst.push_back(word);
-				term_set.insert(word);
-			}
 			// それ以外ならエラー
 			else {
-				cout << "separatorが:=でも::=でもない" << endl;
+				cout << "separatorが=でない" << endl;
 				assert(0);
 			}
 			// TODO: bnf_src_to_dstとbnf_transition_listどっちも共通した過程を踏むのにbnf_transition_listのための関数になっているので調整する
-			bnf_src_to_dst[src].push_back(dst);
-			local_bnf_transition_list.push_back(P_src_dst(src, dst));
+			bnf_transition_list.push_back(P_src_dst(src, dst));
 
 			// bnf_transition_list[term] = {}で初期化
 			for(auto t : term_set){
@@ -58,12 +57,11 @@ vector<P_src_dst> create_bnf_transtion_list(){
 			}
 		}
 	}
-	return local_bnf_transition_list;
 }
 
 
 // 変数の初期化
 void init(){
-	bnf_transition_list = create_bnf_transtion_list();
+	create_bnf_transtion_list();
 	create_nonterm_and_term_list();
 }
