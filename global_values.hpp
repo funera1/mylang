e4e662759d1d38609e9832656f87aa7d6f3eb1b3
funera1 <funeral7776@gmail.com>
2022-04-09 01:52:06 +0900

    add dfa_graphs

--- global_values.hpp
+++ global_values.hpp
@@ -3,6 +3,8 @@
 // あるtokenから遷移するtoken列のリスト(NUSED+1はtokenkindの個数)
 // 例) tokenTransTable[0] = P_Src_Dst("PROGRAM", {"LBRACKET", "COMPOUND", "RBRACKET"})
 vector<P_src_dst> bnf_transition_list;
+// dstの移動先のリスト
+vector< vector<vector<string>> >dfa_graphs;
 // トークンの文字列から対応するtokenkindを割り当てる
 map<string, int> tokenstr_to_tokenkind;
 // map<string, vector<vector<string>>> bnf_src_to_dst;
